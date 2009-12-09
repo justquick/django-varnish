0ea981e4b5b7458c39a5cfa268dd3ae55a1a7248
Justin Quick <justquick@gmail.com>
2009-12-09 04:06:46 -0500

    new setup≈

--- setup.py
+++ setup.py
@@ -5,6 +5,8 @@ setup(
     version = '0.1',
     url = 'http://opensource.washingtontimes.com/projects/django-varnish/',
     author = 'Justin Quick',
+    author_email= 'justquick@gmail.com',
+    long_description=open('README.rst').read(),
     description = 'Integration between Django and the Varnish HTTP accelerator using the management port using telnet',
     packages = ['varnishapp']
 )
--- varnish/__init__.py
+++
@@ -1,7 +0,0 @@
-import telnetlib
-
-url = '/sports/'
-
-tn = telnetlib.Telnet('172.16.12.68',82)
-tn.write("purge.url %s\n"%url)
-assert tn.read_until('\n\n').startswith('200')
